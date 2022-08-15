8813c63b993634490919d320fc6ad775ebb0acf9
phuocday <phuocday@gmail.com>
2022-08-15 22:19:44 +0700

    a

--- TestAPI.js
+++ TestAPI.js
@@ -13,7 +13,7 @@ let app = {
         }
     },
     render: function (app){
-        return fetch(`https://youtube.googleapis.com/youtube/v3/search?q=${app.querry}&maxResults=10&key=${key}`)
+        return fetch(`https://youtube.googleapis.com/youtube/v3/search?q=${app.querry}&key=${key}`)
                 .then(response=>{
                 return  response.json()
                 }
